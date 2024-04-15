3a7b062af9705bd4c0fb68866a70d4bf65147bf9
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-04-15 12:08:48 +0700

    learn variables

---
+++ udemy-pzn/fundamental/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Aditya"
+	var lastName = "Prabowo"
+	age := 23
+
+	var (
+		favBand                string = "Dream Theater"
+		favProgrammingLanguage        = "Go"
+		favNumber              uint8  = 7
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(age)
+	fmt.Println(favBand)
+	fmt.Println(favProgrammingLanguage)
+	fmt.Println(favNumber)
+}
