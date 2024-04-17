09153a971b032b818757c524bbf7ad5771c362f8
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-04-17 14:21:36 +0700

    learn comparison operations

---
+++ udemy-pzn/fundamental/comparison-operations.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var A string = "Dita"
+	var B string = "Dito"
+
+	fmt.Println(A == B)
+	fmt.Println(A != B)
+	fmt.Println(A < B)
+
+	fmt.Println(10 < 10)
+	fmt.Println(10 > 10)
+	fmt.Println(10 >= 10)
+	fmt.Println(10 <= 10)
+	fmt.Println(1 < 5)
+	fmt.Println(5 > 1)
+}
