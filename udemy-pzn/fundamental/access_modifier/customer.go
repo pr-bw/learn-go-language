cd8d1a846010bf48de1e2de573ba4769127f801a
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-04-26 12:33:57 +0700

    learn access modifier

---
+++ udemy-pzn/fundamental/access_modifier/customer.go
@@ -0,0 +1,14 @@
+package access_modifier
+
+import "fmt"
+
+var name string = "Aditya"
+var Age uint8 = 10
+
+func SayHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func sayGoodbyeWorld() {
+	fmt.Println("Goodbye World")
+}
---
+++ udemy-pzn/fundamental/accessmodifier.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"fundamental/access_modifier"
+)
+
+func main() {
+	fmt.Println(access_modifier.Age)
+	access_modifier.SayHelloWorld()
+}
