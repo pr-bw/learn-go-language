653e18e2e6e5f09fdf5dff134da08f8705b4f3d7
Aditya Prabowo <adityaprabowo.dev@gmail.com>
2024-04-26 12:10:46 +0700

    learn package and import

---
+++ udemy-pzn/fundamental/helper/helper.go
@@ -0,0 +1,5 @@
+package helper
+
+func SayGoodbye() string {
+	return "Goodbye"
+}
---
+++ udemy-pzn/fundamental/import.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"fundamental/helper"
+)
+
+func main() {
+	goodbye := helper.SayGoodbye()
+
+	fmt.Println(goodbye)
+}
